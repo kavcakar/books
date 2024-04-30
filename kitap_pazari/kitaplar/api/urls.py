f24b3c8bcb3b9f73a3ffde2c1af217c3baa84696
Emre kavcakar <kavcakaremre@gmail.com>
2024-04-30 14:39:56 -0400

    concrete view

--- kitap_pazari/kitaplar/api/urls.py
+++ kitap_pazari/kitaplar/api/urls.py
@@ -3,4 +3,5 @@ from kitaplar.api import views as api_views
 
 urlpatterns = [
     path('kitaplar/',api_views.KitapListCreateAPIView.as_view(), name='kitap-listesi'),
+    path('kitaplar/<int:pk>', api_views.KitapDetailAPIView.as_view(), name='kitap-bilgileri'),
 ]
