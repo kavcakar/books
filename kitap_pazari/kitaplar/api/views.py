45d8c835685f8489fb70dfeee696ced65ab194ca
Emre kavcakar <kavcakaremre@gmail.com>
2024-04-30 14:05:32 -0400

    genericapiview

--- kitap_pazari/kitaplar/api/views.py
+++ kitap_pazari/kitaplar/api/views.py
@@ -4,10 +4,15 @@ from rest_framework.mixins import ListModelMixin, CreateModelMixin
 from kitaplar.api.serializers import KitapSerializer, YorumSerializer
 from kitaplar.models import Kitap
 
-class KitapListCreateAPI(GenericAPIView):
+class KitapListCreateAPIView(ListModelMixin, CreateModelMixin, GenericAPIView):
     
     queryset = Kitap.objects.all()
-    serializer_class =KitapSerializer
+    serializer_class = KitapSerializer
+    
     #Listelemek
     def get(self, request, *args, **kwargs):
-        return self,list(request, *args, **kwargs)
+        return self.list(request, *args, **kwargs)
+
+    #Yaratabilmek
+    def post(self,request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
