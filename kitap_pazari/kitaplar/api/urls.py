08659f347e291094522f84ea4246aeeb347231c1
Emre kavcakar <kavcakaremre@gmail.com>
2024-05-01 00:41:50 -0400

    yorum

--- kitap_pazari/kitaplar/api/urls.py
+++ kitap_pazari/kitaplar/api/urls.py
@@ -4,4 +4,5 @@ from kitaplar.api import views as api_views
 urlpatterns = [
     path('kitaplar/',api_views.KitapListCreateAPIView.as_view(), name='kitap-listesi'),
     path('kitaplar/<int:pk>', api_views.KitapDetailAPIView.as_view(), name='kitap-bilgileri'),
+    path('kitaplar/<int:kitap_pk>/yorum_yap', api_views.YorumCreateAPIView.as_view(), name='yorum-yap'),
 ]
--- kitap_pazari/kitaplar/api/views.py
+++ kitap_pazari/kitaplar/api/views.py
@@ -2,9 +2,10 @@ from rest_framework.generics import GenericAPIView
 from rest_framework.mixins import ListModelMixin, CreateModelMixin
 
 from rest_framework import generics
+from rest_framework.generics import get_object_or_404
 
 from kitaplar.api.serializers import KitapSerializer, YorumSerializer
-from kitaplar.models import Kitap
+from kitaplar.models import Kitap, Yorum
 
 class KitapListCreateAPIView(generics.ListCreateAPIView):
     queryset = Kitap.objects.all()
@@ -14,6 +15,16 @@ class KitapListCreateAPIView(generics.ListCreateAPIView):
 class KitapDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Kitap.objects.all()
     serializer_class = KitapSerializer
+
+# Kitapba yorumlara yonlendirmek icin burayi yaptik hangi kitaba hangi yorum gidecek
+class YorumCreateAPIView(generics.CreateAPIView):
+    queryset = Yorum.objects.all()
+    serializer_class = YorumSerializer
+    def perform_create(self, serializer):
+        kitap_pk = self.kwargs.get('kitap_pk')
+        kitap = get_object_or_404(Kitap, pk=kitap_pk)
+        serializer.save(kitap=kitap)
+
 
 # class KitapListCreateAPIView(ListModelMixin, CreateModelMixin, GenericAPIView):
     
